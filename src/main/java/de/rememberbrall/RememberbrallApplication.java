c155df8795e20e7428e6e15410b24685379f743d
Eva Goettert <eva.goettert@idealo.de>
2017-08-01 22:05:59 +0200

    convert to maven project

---
+++ src/main/java/de/rememberbrall/RememberbrallApplication.java
@@ -0,0 +1,12 @@
+package de.rememberbrall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberbrallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RememberbrallApplication.class, args);
+	}
+}
---
+++ src/test/java/de/rememberbrall/RememberbrallApplicationTests.java
@@ -0,0 +1,16 @@
+package de.rememberbrall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RememberbrallApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
