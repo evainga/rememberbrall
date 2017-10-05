c57bf11743cc4ee6866228df8a5d7bde51207b37
Eva Goettert <eva.goettert@idealo.de>
2017-10-05 22:25:30 +0200

    cleanup

--- src/main/java/de/rememberbrall/RememberbrallApplication.java
+++ src/main/java/de/rememberbrall/RememberbrallApplication.java
@@ -1,37 +1,16 @@
 package de.rememberbrall;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@ComponentScan(basePackages = "de.rememberbrall") //ask Marco or test without
+@ComponentScan(basePackages = "de.rememberbrall")
 public class RememberbrallApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RememberbrallApplication.class, args);
-    }
-}
-
-@RestController
-class ServiceInstanceRestController {
-
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-    @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
-            @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
     }
 }
