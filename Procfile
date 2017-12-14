281f10165b2697bdcd92f11af88a11af34fc93ae
Eva Goettert <eva.goettert@idealo.de>
2017-12-14 14:24:37 +0100

    Add profiles local and production

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: java $JAVA_OPTS -Dserver.port=$PORT -jar target/*.jar
+web: java $JAVA_OPTS -Dserver.port=$PORT -jar target/*.jar -Dspring.profiles.active=production
 
--- src/main/java/de/rememberbrall/RememberbrallApplication.java
+++ src/main/java/de/rememberbrall/RememberbrallApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 @SpringBootApplication
 @ComponentScan(basePackages = "de.rememberbrall")
 @EnableReactiveMongoRepositories
+//@PropertySource("application-${spring.profiles.active}.properties")
 public class RememberbrallApplication {
 
     public static void main(String[] args) {
