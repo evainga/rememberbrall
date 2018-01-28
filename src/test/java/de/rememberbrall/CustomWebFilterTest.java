92344ea7d3d2958a752dcddf1179be173067ae25
Eva Goettert <eva.goettert@idealo.de>
2018-01-28 23:27:55 +0100

    Make tests independent from each other

--- src/test/java/de/rememberbrall/CustomWebFilterTest.java
+++ src/test/java/de/rememberbrall/CustomWebFilterTest.java
@@ -1,26 +1,21 @@
 package de.rememberbrall;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
 import org.springframework.mock.web.server.MockServerWebExchange;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilterChain;
 import org.testng.annotations.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 public class CustomWebFilterTest extends MockitoTest {
-
-    @Mock
-    private WebFilterChain webFilterChain;
 
     @InjectMocks
     private CustomWebFilter customWebFilter;
@@ -29,15 +24,16 @@ public class CustomWebFilterTest extends MockitoTest {
     @Test
     public void filterForRootUrl() {
         //Given
+        WebFilterChain webFilterChain = mock(WebFilterChain.class);
         MockServerHttpRequest mockServerHttpRequest = MockServerHttpRequest.get("/").build();
         MockServerWebExchange mockServerWebExchange = MockServerWebExchange.from(mockServerHttpRequest);
 
         //When
         customWebFilter.filter(mockServerWebExchange, webFilterChain);
- 
+
         //Then
         ArgumentCaptor<ServerWebExchange> captor = ArgumentCaptor.forClass(ServerWebExchange.class);
-        verify(webFilterChain, times(2)).filter(captor.capture());
+        verify(webFilterChain).filter(captor.capture());
         ServerWebExchange webExchange = captor.getValue();
         assertThat(webExchange.getRequest().getPath().pathWithinApplication().value()).isEqualTo("/docs/index.html");
     }
@@ -45,6 +41,7 @@ public class CustomWebFilterTest extends MockitoTest {
     @Test
     public void filterForAllOtherUrls() {
         //Given
+        WebFilterChain webFilterChain = mock(WebFilterChain.class);
         MockServerHttpRequest mockServerHttpRequest = MockServerHttpRequest.options("/dummyUrl").build();
         MockServerWebExchange mockServerWebExchange = MockServerWebExchange.from(mockServerHttpRequest);
         when(webFilterChain.filter(mockServerWebExchange)).thenReturn(Mono.empty());
